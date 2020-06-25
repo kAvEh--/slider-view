ea1a643670d925e4ec15e912e919818e6bf496a6
kaveh <kaveh.fazaeli@gmail.com>
2020-06-25 13:26:51 +0430

    First Commit

---
+++ app/src/main/java/com/kaveh/slider/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kaveh.slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
